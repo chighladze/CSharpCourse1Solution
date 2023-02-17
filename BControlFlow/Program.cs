60f11c4939c527f0a38b54b9ecfc997622e53b01
Giorgi Chighladze <80564127+chighladze@users.noreply.github.com>
2023-02-17 11:15:56 +0400

    NestedFor

--- BControlFlow/Program.cs
+++ BControlFlow/Program.cs
@@ -6,9 +6,32 @@ namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-        static void Main(string[] args) 
+        static void Main(string[] args)
         {
 
+        }
+        static void NestedFor() 
+        {
+            int[] numbers = { 1, -2, 4, -7, 5, 3, 2, -1, -3, 2, 7, -1, -3, 1, 7 };
+
+            for (int i =0; i < numbers.Length - 2; i++)
+            {
+                for (int j = i +1; j < numbers.Length - 1; j++)
+                {
+                    for (int k = j + 1; k < numbers.Length; k++)
+                    {
+
+                        int atI = numbers[i];
+                        int atJ = numbers[j];
+                        int atK = numbers[k];
+
+                        if (atI + atJ + atK == 0)
+                        {
+                            Console.WriteLine($"Pair ({atI};{atJ};{atK}). Indexes ({i};{j};{k})");
+                        }
+                    }
+                }
+            }
         }
 
         static void ForForeach()
