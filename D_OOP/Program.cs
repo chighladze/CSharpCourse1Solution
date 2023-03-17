a8f28aba42ca65fd27e301e6b0e723d8a6ff7fb7
Giorgi Chighladze <80564127+chighladze@users.noreply.github.com>
2023-03-17 23:20:48 +0400

    RomanNumeral

--- D_OOP/Program.cs
+++ D_OOP/Program.cs
@@ -4,10 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Calculator calc = new Calculator();
-            double square1 = calc.CalcTriangleSquare(10, 10, 10);
-            double square2 = calc.CalcTriangleSquare(10, 10);
-            Console.WriteLine($"Square1={square1}. Square2={square2}");
+            // Calculator calc = new Calculator();
+            // double square1 = calc.CalcTriangleSquare(10, 10, 10);
+            // double square2 = calc.CalcTriangleSquare(10, 10);
+            // Console.WriteLine($"Square1={square1}. Square2={square2}");
+
+
+            Console.WriteLine(RomanNumeral.Parse("LXIV"));
 
         }
     }
---
+++ D_OOP/RomanNumeral.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_OOP
+{
+    class RomanNumeral
+    {
+        private static Dictionary<char, int> map = new Dictionary<char, int>()
+        {
+            {'I', 1 },
+            {'V', 5 },
+            {'X', 10 },
+            {'L', 50 },
+            {'C', 100 },
+            {'D', 500 },
+            {'M', 1000 },
+        };
+
+        // content of the projection of Roman numbers into Arabic.
+        public static int Parse(string roman)
+        {
+            int result = 0;
+
+            for (int i = 0; i < roman.Length; i++)
+            {
+                if (i + 1 < roman.Length && isSubtractive(roman[i], roman[i + 1]))
+                {
+                    result -= map[roman[i]];
+                }
+                else
+                {
+                    result += map[roman[i]];
+                }
+            }
+
+            return result;
+        }
+
+        private static bool isSubtractive(char c1, char c2)
+        {
+            return map[c1] < map[c2];
+        }
+
+    }
+}
