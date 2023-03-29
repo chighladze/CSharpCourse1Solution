cbf2759a1689b46ba80e0a9597acf0d14d7073c6
Giorgi Chighladze <80564127+chighladze@users.noreply.github.com>
2023-03-29 16:15:11 +0400

    Average2

--- D_OOP/Calculator.cs
+++ D_OOP/Calculator.cs
@@ -8,6 +8,30 @@ namespace D_OOP
 {
     public class Calculator
     {
+        public double Average(int[] numbers)
+        {
+            double sum = 0;
+
+            foreach (int n in numbers)
+            {
+                sum += n;
+            }
+            return sum / numbers.Length;
+        }
+
+        public double Average2(params int[] numbers)
+        {
+            double sum = 0;
+
+            foreach (int n in numbers)
+            {
+                sum += n;
+            }
+            return sum / numbers.Length;
+        }
+
+
+
         public double CalcTriangleSquare(double ab, double bc, double ac)
         {
             double p = (ab + bc + ac) / 2;
@@ -27,7 +51,11 @@ namespace D_OOP
         public double CalcTriangleSquare(double ab, double ac, int alpha)
         {
             double rads = alpha * Math.PI / 180;
+
             return 0.5 * ab * ac * Math.Sin(rads);
         }
     }
 }
+
+
+
--- D_OOP/Program.cs
+++ D_OOP/Program.cs
@@ -5,15 +5,17 @@
         static void Main(string[] args)
         {
             Calculator calc = new Calculator();
+
             // double square1 = calc.CalcTriangleSquare(10, 10, 10);
             // double square2 = calc.CalcTriangleSquare(10, 10);
             // Console.WriteLine($"Square1={square1}. Square2={square2}");
 
+            //Console.WriteLine(RomanNumeral.Parse("LXIV"));
+            //double square = calc.CalcTriangleSquare(10, 20, 50);
+            //Console.WriteLine(square);
 
-            Console.WriteLine(RomanNumeral.Parse("LXIV"));
-
-            double square = calc.CalcTriangleSquare(10, 20, 50);
-            Console.WriteLine(square);
+            double avg = calc.Average2(1, 2, 3, 4);
+            Console.WriteLine(avg);
 
         }
     }
