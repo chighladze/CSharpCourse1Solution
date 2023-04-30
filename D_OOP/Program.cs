0795b33e7965d6dcde94358ff6997ecb50c9d664
Giorgi Chighladze <80564127+chighladze@users.noreply.github.com>
2023-04-30 22:26:41 +0400

    TicTacToeGame

--- D_OOP/Program.cs
+++ D_OOP/Program.cs
@@ -5,23 +5,58 @@ using Microsoft.VisualBasic;
 using System.Collections.Generic;
 using System.Reflection.Metadata;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using D_OOP.Homeworks;
-using D_OOP.HomeWorks;
+using D_OOP.Homeworks.TicTacToeGame;
+
 
 
 namespace D_OOP
 {
     class Program
     {
+
+        private static TicTacToeGame g = new TicTacToeGame();
         static void Main(string[] args)
         {
-            GuessNumberGame game = new GuessNumberGame(guessingPlayer:GuessingPlayer.Machine);
-            game.Start();
+            Console.WriteLine(GetPrintableState());
 
-            Console.WriteLine();
+            while (g.GetWinner() == Winner.GameIsUnfinished)
+            {
+                int index = int.Parse(Console.ReadLine());
+                g.MakeMove(index);
+
+                Console.WriteLine();
+                Console.WriteLine(GetPrintableState());
+            }
+
+            Console.WriteLine($"Result: {g.GetWinner()}");
+            Console.ReadLine();
 
 
         }
+
+        static string GetPrintableState()
+        {
+            var sb = new StringBuilder();
+            for (int i = 1; i <= 7; i += 3)
+            {
+                sb.AppendLine("     |     |     |")
+                    .AppendLine(
+                        $"  {GetPrintableChar(i)}  |  {GetPrintableChar(i + 1)}  |  {GetPrintableChar(i + 2)}  |")
+                    .AppendLine("_____|_____|_____|");
+            }
+            return sb.ToString();
+        }
+
+        static string GetPrintableChar(int index)
+        {
+            State state = g.GetState(index);
+            if (state == State.Unset)
+                return index.ToString();
+            return state == State.Cross ? "X" : "O";
+        }
+
 
         static void MyStack()
         {
