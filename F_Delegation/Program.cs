525f15917cc85d57ed694b1c78a59d633eab087d
Giorgi Chighladze <80564127+chighladze@users.noreply.github.com>
2023-05-09 22:48:52 +0400

    Delegate

--- F_Delegation/Program.cs
+++ F_Delegation/Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace F_Delegation
+{
+
+    class Car
+    {
+        private int speed = 0;
+
+        public delegate void TooFast();
+
+        private TooFast tooFast;
+
+        public void Start()
+        {
+            speed = 10;
+        }
+
+        public void Accelerate()
+        {
+            speed += 10;
+
+            if (speed > 80)
+            {
+                tooFast();
+            }
+        }
+
+        public void Stop()
+        {
+            speed = 0;
+        }
+
+        public void RegisterOnTooFast(TooFast tooFast)
+        {
+            this.tooFast = tooFast;
+        }
+
+    }
+
+    class Program
+    {
+        private static Car car;
+
+        static void Main(string[] args)
+        {
+            car = new Car();
+            car.RegisterOnTooFast(HandleOnTooFast);
+
+            car.Start();
+
+            car.Accelerate();
+            car.Accelerate();
+            car.Accelerate();
+
+        }
+
+        private static void HandleOnTooFast()
+        {
+            Console.WriteLine("Oh, I got it, calling stop!");
+            car.Stop();
+        }
+    }
+}
